a8ed67a23dc45631600d0f244659d275c7ecccc5
mrzushiza <mrzushiza@users.noreply.github.com>
2026-09-18 12:29:12 +0200

    Sync Shopify CLI generated configuration

--- extensions/customer-match-sessions/shopify.d.ts
+++ extensions/customer-match-sessions/shopify.d.ts
@@ -1,6 +1,6 @@
 import '@shopify/ui-extensions';
 
-//@ts-expect-error Shopify injects the target-specific API type at extension runtime.
+//@ts-ignore
 declare module './src/BlockExtension.jsx' {
   const shopify: import('@shopify/ui-extensions/admin.customer-details.block.render').Api;
   const globalThis: { shopify: typeof shopify };
